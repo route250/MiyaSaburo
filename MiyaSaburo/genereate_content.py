92f572d1c524fc1f407ea6f41c01fdff53bdf98f
route250@mx130 <hanaya.white@gmail.com>
2023-09-03 12:06:03 +0900

    update tweet post

--- MiyaSaburo/genereate_content.py
+++ MiyaSaburo/genereate_content.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 import tweepy
+import tweepy.errors
 from PIL import Image
 from io import BytesIO
 from tools.webSearchTool import WebSearchModule
@@ -168,16 +169,18 @@ def neko_news():
     dates = [ Utils.date_today(), Utils.date_today(-1), Utils.date_today(-2)]
 
     dates =  Utils.date_today()
-    LANG_JP='jp'
-    lang=LANG_JP
-    if lang == LANG_JP:
-        query = f"猫の話題 -site:www.youtube.com after: {dates}"
-    else:
-        query = f"Funny Cat News stories -site:www.youtube.com after: {dates}"
-
+    LANG_JP='Japanese'
+    query_jp = f"猫の話題 -site:www.youtube.com after: {dates}"
+    query_en = f"Funny Cat News stories -site:www.youtube.com after: {dates}"
     n_return = 10
-    search_results = module.search_meta( query, num_result = n_return )
+    print( f"検索中: {query_jp}")
+    search_results_jp = module.search_meta( query_jp, num_result = n_return )
+    print( f"検索中: {query_en}")
+    search_results_en = module.search_meta( query_en, num_result = n_return )
     # print( f"result:{len(search_results)}")
+    search_results = [x for pair in zip(search_results_jp, search_results_en) for x in pair]
+    search_results.extend(search_results_jp[len(search_results_en):])  # AがBより長い場合の残りの要素
+    search_results.extend(search_results_en[len(search_results_jp):])  # BがAより長い場合の残りの要素
 
     detect_fmt = "下記の記事内容について\n\n記事タイトル:{}\n記事内容:\n{}\n\n"
     detect_fmt = f"{detect_fmt}下記の項目に答えて下さい。\n"
@@ -205,7 +208,7 @@ def neko_news():
         "・投稿を見た人が興味を持つ内容",
         "・投稿を見た人から信頼を得られる内容",
         "・カジュアルな口調で",
-        "・野良猫がニュースを見た感想を述べる",
+        "・猫がニュースを見た感想を述べる",
         # "・最後にCall to Action(いいねなど)を短く入れる",
         "",
         "ターゲット：",
@@ -223,7 +226,7 @@ def neko_news():
         "次に、以下の5つの指標を20点満点で評価してくださいにゃ。",
         "①話題性：ツイートのトピックが、現在の流行やニュースなど、人々が興味を持つ話題に関連しているかにゃ？",
         "②エンゲージメント促進：ツイートが、ユーザーに反応を促すような要素を含んでいるかにゃ？",
-        "③付加価値：野良猫ならではの視点・感想が含まれているかにゃ？",
+        "③付加価値：猫の視点、猫の感想が含まれているかにゃ？",
         "④リアリティ:具体的な例や体験が入り信ぴょう性高く独自性があるかにゃ？",
         "⑤文章の見やすさ:行変えと箇条書きを使い全ての文が20字以内でまとまっているかにゃ？",
         "",
@@ -234,7 +237,7 @@ def neko_news():
         "改善点：",
     ])
 
-    update_prompt = "上記の改善点を踏まえて、野良猫っぽい出力ツイートを{}で{}文字以内で生成するにゃ\n出力文:"
+    update_prompt = "上記の改善点を踏まえて、猫っぽい出力ツイートを{}で{}文字以内で生成するにゃ\n\n出力文:"
 
 
     exclude_site = {
@@ -242,13 +245,34 @@ def neko_news():
         "cat.blogmura.com": 1,
         "www.thoroughbreddailynews.com": 1
     }
+
+    must_keywords = [
+        "猫", "ねこ", "ネコ",
+        "cat", "Cat",
+    ]
+    exclude_keywords = [
+        "記事が見つかりません","公開期間が終了",
+        "販売", "価格", "値段",
+        "譲渡会",
+        "ディズニー", "disney", "Disney",
+        ]
+
+    hashtag_list_jp = [ "#猫", "#cats", "#猫好きさんと繋がりたい","#animals", "#funnyanimals"]
+    hashtag_list_en = [ "#cat", "#CatsAreFamily", "#pets", "#animals", "#funnyanimals"]
+
     site_hist = {}
-    tw_count = 0
+
+    tw_count_max = 1
+    count_jp = 0
+    count_en = 0
     for d in search_results:
         site_title = d.get('title',"")
         site_link = d.get('link',"")
         site_top = urlparse(site_link).netloc
 
+        #---------------------------
+        # 除外判定
+        #---------------------------
         if exclude_site.get( site_top, None ) is not None:
             print( f"Exclude {site_link}")
             continue
@@ -257,57 +281,41 @@ def neko_news():
             print( f"Skip {site_link}")
             continue
 
-        if "https://cat.blogmura.com/cat_picture/" == site_link:
-            continue
-        if len(site_title)==0 or len(site_link)==0:
-            print( f"Error: no title {site_title} {site_link}")
-            continue
-        if "youtube.com" in site_link:
-            print( f"Error: skip youtube {site_title} {site_link}")
-            continue
-        if "karapaia.com" in site_link:
-            print( f"Skip {site_link}")
-            continue
-
         site_text: str = module.get_content( site_link, type="title" )
         if site_text is None or site_text == '':
-            print( f"Error: no article {site_title} {site_link}")
+            print( f"Error: no article\n{site_title}\n{site_link}\n")
             continue
 
-        if site_text.find("記事が見つかりません")>=0 or site_text.find("公開期間が終了")>=0:
-            print( f"Error: no article {site_title} {site_link}")
+        a=[ keyword for keyword in must_keywords if site_text.find(keyword)>=0]
+        if len(a)==0:
+            print( f"Error: not found 猫\n{site_title}\n{site_link}\n")
             print(site_text)
             continue
 
-        if site_text.find("猫")<0 and site_text.find("ねこ")<0 and site_text.find("ネコ")<0 and site_text.find("cats")<0 and site_text.find("Cat")<0:
-            print( f"Error: not found 猫 ")
-            print(site_text)
+        a=[ keyword for keyword in exclude_keywords if site_text.find(keyword)>=0]
+        if len(a)>0:
+            print( f"Skip: found {a}\n{site_title}\n{site_link}\n")
             continue
-
-        if site_text.find("販売")>=0 or site_text.find("価格")>=0 or site_text.find("値段")>=0:
-            print( f"Error: found 販売")
-            print(site_text)
-            continue
-
-        if site_text.find("譲渡会")>=0:
-            print( f"Skip: found 譲渡会")
-            continue
-        if site_text.find("ディズニー")>=0 or site_text.find("disney")>=0  or site_text.find("Disney")>=0:
-            print( f"Skip: found ディズニー")
-            continue
-
         #---------------------------
-        # 記事判定
+        # 記事の内容判定
         #---------------------------
+        print("-------------------------------------------------------")
+        print( f"記事判定:{site_title}")
+        print( f"{site_link}")
+        if count_jp>=tw_count_max and not Utils.contains_kana(site_text):
+            print( f"Skip by lang en {site_title} {site_link}\n" )
+            continue
+        
         detect_hist = []
         detect_hist += [ {"role": "user", "content": detect_fmt.format( site_title, site_text[:2000] ) } ]
         detect_result: str = ChatCompletion(detect_hist)
         if detect_result is None or len(detect_result)<20:
-            print( f"Error: no tweet {site_title} {site_link}\n{detect_result}")
+            print( f"Error: no result for detect {site_title} {site_link}\n{detect_result}")
             continue
-        print( f"記事タイトル:{site_title}")
-        print( f"{site_link}")
+        detect_result = detect_result.replace('\n\n','\n')
+        print("-------------------------------------------------------")
         print(detect_result)
+        print("-------------------------------------------------------")
 
         if find_keyword( detect_result, "Cat", "NotCat" )!=0:
             print( f"Error: 猫記事じゃない {site_title} {site_link}\n" )
@@ -323,37 +331,43 @@ def neko_news():
         # ポスト言語判定
         #---------------------------
         post_lang = find_keyword(detect_result, "InsideJapan", "OutsideJapan")
-        if post_lang==0:
+        if post_lang==0 and Utils.contains_kana(site_text):
+            if count_en>=tw_count_max:
+                print( f"Skip by lang en {site_title} {site_link}\n" )
+                continue
             # 日本のニュースは英語でポスト
             post_lang='English'
             post_limit = 229
         elif post_lang==1:
+            if count_jp>=tw_count_max:
+                print( f"Skip by lang jp {site_title} {site_link}\n" )
+                continue
             # 日本のニュースでないなら日本語でポスト
-            post_lang='Japanese'
+            post_lang=LANG_JP
             post_limit = 129
         #---------------------------
         # 初期生成
         #---------------------------
+        print("-------------------------------------------------------")
+        print( f"生成:{site_title}")
+        print( f"{site_link}")
         msg_hist = []
-        # for title_data,in_data, out_data in examples:
-        #     msg_hist += [ {"role": "system", "content": prompt_fmt.format(title_data) } ]
-        #     msg_hist += [ {"role": "user", "content": in_data } ]
-        #     msg_hist += [ {"role": "assistant", "content": out_data } ]
-
         msg_hist += [ {"role": "user", "content": article_fmt.format( site_title, site_text[:2000] ) } ]
         msg_hist += [ {"role": "system", "content": prompt_fmt.format( post_lang, post_limit) } ]
-
-        base_article = ChatCompletion(msg_hist)
+        base_article = ChatCompletion(msg_hist, temperature=0.7)
         base_article = trim_post( base_article )
         if base_article is None or len(base_article)<20:
-            print( f"Error: no tweet {site_title} {site_link}\n{base_article}")
+            print( f"Error: no tweet {base_article}")
             continue
 
+        tweet_text = None
+        tweet_score = 0
         nn = 0
         nn_max = 3
         nn_limit = 5
-        print(f"{nn}回目ツイート内容\n{base_article}")
+        print(f"{nn}回目ツイート内容:{site_link}\n{base_article}")
 
+        msg_start = len(msg_hist)
         while nn <= nn_max and nn <= nn_limit:
             nn += 1
             base_article0 = base_article
@@ -367,78 +381,80 @@ def neko_news():
                 ]
                 evaluate_response = ChatCompletion(evaluate_msgs)
                 if evaluate_response is None or len(evaluate_response)<20:
-                    print( f"Error: no tweet {site_title} {site_link}\n{evaluate_response}")
-                    continue
+                    print( f"Error: result {site_title} {site_link}\n{evaluate_response}")
+                    break
             else:
-                evaluate_response = f"文字数が{post_limit}文字を超えてるにゃ。"
+                if post_lang == LANG_JP:
+                    evaluate_response = f"文字数が{post_limit}文字を超えてるにゃ。文字数を減らして欲しいにゃ。"
+                else:
+                    evaluate_response = f"The number of characters in the tweet is {len(base_article0)}. Tweet briefly. Tweet within {post_limit} characters. Make it shorter."
                 nn_max += 1
 
             print(f"[{nn}回目評価内容]\n{evaluate_response}")
 
-            pattern = re.compile(r'合計点数.*(\d+)点')
+            #---------------------------
+            # 点数と文字数判定
+            #---------------------------
+            score = 0
+            pattern = re.compile(r'合計点数[^\d]*(\d+)[点/]')
             point_result = pattern.search(evaluate_response)
             if point_result is not None:
-                print(f"検出結果:{point_result.group(0)}")
-                point=point_result.group(1)
-                point=int(point)
-                if point<=90 and point<=100 and len(base_article0)<=post_limit:
-                    break
+                score=int(point_result.group(1))
+                print(f"検出結果:{point_result.group(0)} -> {score}")
+                if score < 0 or 100 < score:
+                    score = 0
+
+            if len(base_article0)<=post_limit:
+                if score>=tweet_score:
+                    tweet_text = base_article0
+                    tweet_score = score
+                    if score>=90 or ( nn>1 and score>=80):
+                        break
 
             #---------------------------
             # ツイートを改善する
             #---------------------------
+            if nn>=3:
+                del msg_hist[msg_start:msg_start+3]
             msg_hist += [ {"role": "assistant", "content": base_article0 } ]
             msg_hist += [ {"role": "system", "content": evaluate_response } ]
-            msg_hist += [ {"role": "system", "content": update_prompt.format( post_lang, post_limit) } ]
+            msg_hist += [ {"role": "user", "content": update_prompt.format( post_lang, post_limit) } ]
 
-            base_article = ChatCompletion(msg_hist)
+            base_article = ChatCompletion(msg_hist, temperature=0.7)
             base_article = trim_post( base_article )
             print(f"[{nn}回目ツイート内容]\n{base_article}")
 
-        tweet_text = base_article
-        p = tweet_text.find("#")
-        hashtag_list = [ "#猫", "#cats", "#猫好きさんと繋がりたい","#animals", "#funnyanimals"]
-        if post_lang == LANG_JP:
-            hashtag_list = [ "#猫", "#cats", "#猫好きさんと繋がりたい","#animals", "#funnyanimals"]
-        else:
-            hashtag_list = [ "#cat", "#CatsAreFamily", "#pets", "#animals", "#funnyanimals"]
+        if Utils.str_length( tweet_text ) < 20:
+            print( f"Error: no tweet" )
+            continue
+        if tweet_text is None or len(tweet_text)>post_limit:
+            print( f"Error: tweet too long" )
+            continue
 
-        if p>1:
-            tweet_text = tweet_text[:p].strip()
-        tweet_tags = " ".join(hashtag_list)
+        if post_lang == LANG_JP:
+            tweet_tags = " ".join(hashtag_list_jp)
+        else:
+            tweet_tags = " ".join(hashtag_list_en)
 
         tx = f"{tweet_text}\n\n{site_link}\n\n{tweet_tags}"
-        chars = len(tx)
         print("----------------------------------------------")
         print( f"{tweet_text}" )
         print( f"{site_link}")
         print( f"{tweet_tags}" )
         print("----------------------------------------------")
 
-        if len(tweet_text) > post_limit:
-            print( f"Error: too long" )
-            continue
-
-        # if tweet_text.find("猫")<0 and tweet_text.find("ねこ")<0 and tweet_text.find("ネコ")<0:
-        #     print( f"Error: not found 猫 ")
-        #     print(tweet_text)
-        #     continue
-
-        # if tweet_text.find("販売")>=0 or tweet_text.find("価格")>=0 or tweet_text.find("値段")>=0:
-        #     print( f"Error: found 販売")
-        #     print(tweet_text)
-        #     continue
-
-        # if tweet_text.find("ノミ")>=0:
-        #     print( f"Error: found ノミ")
-        #     continue
-
         # 投稿
-       # tweeter_client.create_tweet(text=tx)
-        site_hist[site_top] = 1
-        tw_count += 1
-        if tw_count>=2:
-            break
+        try:
+            tweeter_client.create_tweet(text=tx)
+            site_hist[site_top] = 1
+            if post_lang == LANG_JP:
+                count_jp += 1
+            else:
+                count_en += 1
+            if count_jp>=tw_count_max and count_en>=tw_count_max:
+                break
+        except tweepy.errors.BadRequest as ex:
+            print(ex)
 
 def find_keyword( content:str, a:str, b:str ) -> int:
     if content is not None:
@@ -447,20 +463,23 @@ def find_keyword( content:str, a:str, b:str ) -> int:
         if content.find(b)>=0:
             return 1
     return -1
+
 def trim_post( content: str ) -> str:
-    if content.startswith("「") and content.endswith("」"):
-        content = content[1:-1]
-    if content.startswith("「" ) and content.endswith("」"):
-        content = content[1:-1]       
-    content = content.replace("[URL]","")
-    content = content.replace("→","")
+    content = Utils.str_unquote(content)
+    content = re.sub( r'^にゃ[ー〜！ん]+','', content )
+    content = re.sub( r'【[^】]*】',' ', content )
+    content = re.sub( r'\([^)]*\)',' ', content )
+    content = re.sub( r'\[[^\]]*\]',' ', content )
+    content = re.sub( r'https*://','', content )
+    content = re.sub( r' *[a-z0-9._-]*/[a-z0-9./_-]*[ ]*',' ', content )
+    content = re.sub( r"[ ]*[→↓][ ]*"," ", content )
     p = content.find("#")
-    if p>0:
-        content = content[0:p-1]
+    if p>=0:
+        content = content[:p]
     content = content.strip()
     return content
 
-def ChatCompletion( mesg_list ):
+def ChatCompletion( mesg_list, temperature=0 ):
     try:
 
         #print( f"openai.api_key={openai.api_key}")
@@ -469,6 +488,7 @@ def ChatCompletion( mesg_list ):
             openai.api_key=os.getenv('OPENAI_API_KEY')
         response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
+                temperature = temperature,
                 messages=mesg_list
             )
 
@@ -485,10 +505,6 @@ def ChatCompletion( mesg_list ):
         return None
     except Exception as ex:
         traceback.print_exc()
-        return None
-
-    if content is None or len(content)<20:
-        print( f"Error: no tweet \n{content}")
         return None
 
     return content
@@ -524,6 +540,9 @@ def test():
     print(f" d1: {d1}")
     d2 = Utils.date_today(-2)
     print(f" d2: {d2}")
+
+    content = "  \"『 【 \"あいうおえおかき www.yahoo.co.jp/test/sample abcd.efg  】』   \""
+    print( trim_post(content))
 
 if __name__ == '__main__':
 
